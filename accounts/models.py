a6348534a53feda7d47d61795cdf1f42e823fe14
KaitoMizukami <kaito.program@gmail.com>
2022-10-16 15:09:50 +0900

    Fix inheritance of User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser, PermissionManager
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
 )
 
-# Create your models here.
 
-class User(models.Model):
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
