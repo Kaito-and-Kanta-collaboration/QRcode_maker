b67c43253f6aaf8d11015efe80237c9d6d2ff3b1
KaitoMizukami <kaito.program@gmail.com>
2022-10-16 15:32:48 +0900

    Create User model admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from .forms import UserCreationForm, UserChangeForm
 
-# Register your models here.
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username', 'is_staff')
+
+    fieldsets = (
+        ('ユーザユーザー情報', {'fields': ('username', 'password')}),
+        ('パーミッション', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+    )
+
+    add_fieldsets = (
+        ('ユーザユーザー情報', {'fields': ('username',
+         'password', 'confirm_password')}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.unregister(Group)
