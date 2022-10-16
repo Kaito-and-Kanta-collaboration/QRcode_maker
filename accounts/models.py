ef7138e178b855779cd15c79dc75677280c2025d
KaitoMizukami <kaito.program@gmail.com>
2022-10-16 15:08:08 +0900

    Define User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionManager
+)
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
