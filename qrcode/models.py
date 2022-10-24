9c3f2cb80abfc468767d13b97c8d20cb085b38c0
KantaNakamura <0706363@gmail.com>
2022-10-24 19:21:56 +0800

    create create_qrcode base function

--- qrcode/models.py
+++ qrcode/models.py
@@ -7,7 +7,7 @@ from accounts.models import User
 class QRCode(models.Model):
     created_at = models.DateField(default=timezone.now)
     name = models.CharField(max_length=50)
-    qrcode_images = models.FileField(upload_to='qrcode_images/')
+    qrcode_images = models.FileField(upload_to='qrcode_images/', null=True)
     url_or_message = models.CharField(max_length=300, null=True)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE
--- qrcode/views.py
+++ qrcode/views.py
@@ -5,19 +5,31 @@ from .forms import QrcodeCreateForm
 from .models import QRCode
 
 
-class QrcodeCreateView(CreateView):
-    template_name = 'home.html'
-    form_class = QrcodeCreateForm
+# class QrcodeCreateView(CreateView):
+#     template_name = 'home.html'
+#     form_class = QrcodeCreateForm
 
-    def get(self, request):
-        qrcode_form = QrcodeCreateForm()
-        return render(request, 'home.html', context={'create_qrcode_form': qrcode_form})
+#     def get(self, request):
+#         qrcode_form = QrcodeCreateForm()
+#         return render(request, 'home.html', context={'create_qrcode_form': qrcode_form})
 
-    def post(self, request):
-        qrcode_form = QrcodeCreateForm(request.POST)
-        if qrcode_form.is_valid():
-            url_or_message = qrcode_form.cleaned_data['url_or_message']
-            name = qrcode_form.cleaned_data['name']
-            QRCode.create_qrcode(self, url_or_message=url_or_message, name=name)
-            return render(request, 'home.html', context={'create_qrcode_form': qrcode_form})
+#     def post(self, request):
+#         qrcode_form = QrcodeCreateForm(request.POST)
+#         if qrcode_form.is_valid():
+#             url_or_message = qrcode_form.cleaned_data['url_or_message']
+#             name = qrcode_form.cleaned_data['name']
+#             QRCode.create_qrcode(self, url_or_message=url_or_message, name=name)
+#             return render(request, 'home.html', context={'create_qrcode_form': qrcode_form})
+#         return redirect('qrcode:home')
+
+def create_qrcode(request):
+    create_qrcode_form = QrcodeCreateForm(request.POST or None)
+    if create_qrcode_form.is_valid():
+        create_qrcode_form.instance.user = request.user
+        create_qrcode_form.save()
         return redirect('qrcode:home')
+    return render(
+        request, 'home.html', context={
+            'create_qrcode_form': create_qrcode_form
+        }
+    )
