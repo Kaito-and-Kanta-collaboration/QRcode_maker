0efa57baac4cb2a2e37f2060fe0201d82d138454
KantaNakamura <0706363@gmail.com>
2022-10-24 15:54:36 +0800

    add Login_url in settings.py

--- qrcode_maker/settings.py
+++ qrcode_maker/settings.py
@@ -139,3 +139,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/home/'
+LOGOUT_REDIRECT_URL = '/'
