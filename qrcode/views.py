284a3f7a6a7a3ccaf6ba12beae0d40e9bc399a73
KaitoMizukami <kaito.program@gmail.com>
2022-11-01 13:59:43 +0900

    Delete unnecessary comments

--- qrcode/views.py
+++ qrcode/views.py
@@ -1,31 +1,11 @@
 from django.shortcuts import redirect, render
-from django.views.generic.edit import CreateView
 from django.core.files import File
-from django.urls import reverse_lazy
-from django.http import HttpResponseRedirect
 
 import pyqrcode
 
 from .forms import QrcodeCreateForm
 from .models import QRCode
 
-
-# class QrcodeCreateView(CreateView):
-#     template_name = 'home.html'
-#     form_class = QrcodeCreateForm
-
-#     def get(self, request):
-#         qrcode_form = QrcodeCreateForm()
-#         return render(request, 'home.html', context={'create_qrcode_form': qrcode_form})
-
-#     def post(self, request):
-#         qrcode_form = QrcodeCreateForm(request.POST)
-#         if qrcode_form.is_valid():
-#             url_or_message = qrcode_form.cleaned_data['url_or_message']
-#             name = qrcode_form.cleaned_data['name']
-#             QRCode.create_qrcode(self, url_or_message=url_or_message, name=name)
-#             return render(request, 'home.html', context={'create_qrcode_form': qrcode_form})
-#         return redirect('qrcode:home')
 
 def create_qrcode(request):
     create_qrcode_form = QrcodeCreateForm(request.POST or None)
@@ -44,11 +24,6 @@ def create_qrcode(request):
         # get create qrcode
         show_qrcode = QRCode.objects.filter(user=request.user, name=create_qrcode_form.cleaned_data['name']).first()
         print(show_qrcode.qrcode_images.url)
-        # return HttpResponseRedirect(
-        #     reverse_lazy('qrcode:home', kwargs={
-        #         'show_qrcode': show_qrcode
-        #     })
-        # )
         return render(
             request, 'home.html', context={
                 'create_qrcode_form': create_qrcode_form,
