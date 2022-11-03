74e10960184999d692744aeae08211b5e8291cfc
KaitoMizukami <kaito.program@gmail.com>
2022-11-03 14:30:32 +0900

    Add remove to delete created img on project

--- qrcode/views.py
+++ qrcode/views.py
@@ -1,7 +1,7 @@
+import os
+
 from django.shortcuts import redirect, render
 from django.core.files import File
-
-import pyqrcode
 
 from .forms import QrcodeCreateForm
 from .models import QRCode
@@ -11,19 +11,20 @@ def create_qrcode(request):
     create_qrcode_form = QrcodeCreateForm(request.POST or None)
     if create_qrcode_form.is_valid():
         create_qrcode_form.instance.user = request.user
-        # create qrcode
-        qrcode_name = create_qrcode_form.cleaned_data['name']+'.png'
-        created_qrcode = pyqrcode.create(create_qrcode_form.cleaned_data['url_or_message'])
-        created_qrcode.png(qrcode_name, scale=10)
+        
+        qrcode_name = create_qrcode_form.cleaned_data['name']
+        url_or_message = create_qrcode_form.cleaned_data['url_or_message']
+        qrcode_name = create_qrcode_form.create_qrcode(url_or_message, qrcode_name)
         
         destination_file = open(qrcode_name, 'rb')
         create_qrcode_form.instance.qrcode_images.save(qrcode_name, File(destination_file), save=False)
         destination_file.close()
+
+        os.remove(qrcode_name)
         
         create_qrcode_form.save()
         # get create qrcode
-        show_qrcode = QRCode.objects.filter(user=request.user, name=create_qrcode_form.cleaned_data['name']).first()
-        print(show_qrcode.qrcode_images.url)
+        show_qrcode = QRCode.objects.get(name=create_qrcode_form.cleaned_data['name'])
         return render(
             request, 'home.html', context={
                 'create_qrcode_form': create_qrcode_form,
