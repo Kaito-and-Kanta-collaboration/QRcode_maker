f195077b15ae271bc33685fd9895f308c49b93db
KaitoMizukami <kaito.program@gmail.com>
2022-11-03 14:38:35 +0900

    Refactoring using with keyword

--- qrcode/views.py
+++ qrcode/views.py
@@ -15,24 +15,24 @@ def create_qrcode(request):
         qrcode_name = create_qrcode_form.cleaned_data['name']
         url_or_message = create_qrcode_form.cleaned_data['url_or_message']
         qrcode_name = create_qrcode_form.create_qrcode(url_or_message, qrcode_name)
-        
-        destination_file = open(qrcode_name, 'rb')
-        create_qrcode_form.instance.qrcode_images.save(qrcode_name, File(destination_file), save=False)
-        destination_file.close()
 
-        os.remove(qrcode_name)
+        with open(qrcode_name, 'rb') as destination_file:
+            create_qrcode_form.instance.qrcode_images.save(qrcode_name, File(destination_file), save=False)
+
+        os.remove(qrcode_name) # Delete created image on base directory
         
         create_qrcode_form.save()
-        # get create qrcode
+
+        # Get created image to display it
         show_qrcode = QRCode.objects.get(name=create_qrcode_form.cleaned_data['name'])
+
         return render(
             request, 'home.html', context={
                 'create_qrcode_form': create_qrcode_form,
                 'show_qrcode': show_qrcode
             }
         )
+
     return render(
-        request, 'home.html', context={
-            'create_qrcode_form': create_qrcode_form,
-        }
+        request, 'home.html',{ 'create_qrcode_form': create_qrcode_form,}
     )
